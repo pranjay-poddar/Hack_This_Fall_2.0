97d184a96647e74f15a053d36298f8d83f4e1b50
Subhojeet <65619609+Subho0710@users.noreply.github.com>
2021-10-23 00:04:56 +0530

    added self-analysis component

---
+++ src/app/common-services/self-analysis/self-analysis.component.ts
@@ -0,0 +1,110 @@
+import { Component, OnInit } from '@angular/core';
+import { Option, Question, Quiz, QuizConfig } from '../../models/index';
+import { HelperService } from 'src/app/services/helper.service';
+import { flyInOut , expand} from '../../Utilities/animations/animation';
+import { QuizService } from 'src/app/services/quiz.service';
+import { SharingService } from 'src/app/services/sharing.service';
+let positive = 0;
+let answers:any = [];
+
+@Component({
+  selector: 'app-self-analysis',
+  templateUrl: './self-analysis.component.html',
+  styleUrls: ['./self-analysis.component.scss'],
+  providers: [QuizService],
+    animations: [
+      flyInOut(),
+      expand()
+    ]
+})
+
+export class SelfAnalysisComponent implements OnInit {
+  hidecomponent = "cat";
+  analysisResultPositive:string='';
+  analysisResultNegative:string='';
+  heightfix="heightfix";
+  marginHide = "heading-1";
+  quizes !: any[];
+  quiz: Quiz = new Quiz(null);
+  mode = 'quiz';
+  quizName !: string;
+  light ! : string;
+  config: QuizConfig = {
+    'allowBack': true,
+    'allowReview': true,
+    'autoMove': true,  // if true, it will move to next question automatically when answered.
+    'duration': 600,  // indicates the time (in secs) in which quiz needs to be completed. 0 means unlimited.
+    'pageSize': 1,
+    'requiredAll': true,  // indicates if you must answer all the questions before submitting.
+    'richText': false,
+    'shuffleQuestions': true,
+    'shuffleOptions': false,
+    'showClock': false,
+    'showPager': true,
+    'theme': 'none'
+  };
+
+  pager = {
+    index: 0,
+    size: 1,
+    count: 1
+  };
+  timer: any = null;
+  startTime !: Date;
+  endTime !: Date;
+  ellapsedTime = '00:00';
+  duration = '';
+  constructor(private quizService: QuizService, private sharingService:SharingService) { }
+
+  ngOnInit() {
+    this.quizes = this.quizService.getAll();
+    this.quizName = this.quizes[0].id;
+    this.loadQuiz(this.quizName);
+    if(this.quizes[0]){
+    
+    }
+    this.light = this.sharingService.getData();
+  }
+  
+
+  get filteredQuestions() {
+    return (this.quiz.questions) ?
+      this.quiz.questions.slice(this.pager.index, this.pager.index + this.pager.size) : [];
+  }
+
+  onSelect(question: any, option: any) {
+    if (question.QuestionTypeId == 1) {
+      question.Options.forEach((x:any) => { if (x.Id != option.Id) x.Selected = false; });
+    }
+
+    if (this.config.autoMove) {
+      this.goTo(this.pager.index + 1);
+    }
+  }
+
+  goTo(index: number) {
+    if (index >= 0 && index < this.pager.count) {
+      this.pager.index = index;
+      this.mode = 'quiz';
+    }
+  }
+
+  isAnswered(question: Question) {
+
+    return question.options.find(x => x.selected) ? 'Answered' : 'Not Answered';
+    
+  };
+  
+  
+
+  onPositive(){
+    // console.log(positive);
+    if(positive >= 6){
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  }
+
+}
